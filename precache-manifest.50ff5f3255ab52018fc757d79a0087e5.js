382adbf4123410aad53596086aebfcec5243443e
Diana Dudak <DDudak@luxoft.com>
2020-08-23 23:22:57 +0300

    builded page

---
+++ precache-manifest.50ff5f3255ab52018fc757d79a0087e5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b745e19f62b26b7edbbf6088a1edcdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d8a6e4dc3b4d8956c9b",
+    "url": "/static/css/2.cd67af34.chunk.css"
+  },
+  {
+    "revision": "2d8a6e4dc3b4d8956c9b",
+    "url": "/static/js/2.4e9df8a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4e9df8a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1696f2c5e05223af9ba6",
+    "url": "/static/js/main.34914559.chunk.js"
+  },
+  {
+    "revision": "45ee63038faf7d941adb",
+    "url": "/static/js/runtime-main.eb30d0ca.js"
+  },
+  {
+    "revision": "4f6e34b236f56c03f63ba23729bba179",
+    "url": "/static/media/1.4f6e34b2.png"
+  },
+  {
+    "revision": "eccc97521b4301906fe8cf1a820e120d",
+    "url": "/static/media/10.eccc9752.png"
+  },
+  {
+    "revision": "629d5e5580a0bc50f310d5c64099ef77",
+    "url": "/static/media/11.629d5e55.png"
+  },
+  {
+    "revision": "e9ba0893d559d53cd953ca1c2dab72f3",
+    "url": "/static/media/2.e9ba0893.png"
+  },
+  {
+    "revision": "7520e66a4acfaf3492c6e38d4b3b5978",
+    "url": "/static/media/3.7520e66a.png"
+  },
+  {
+    "revision": "c2c42020abc46f210b94ba29356be575",
+    "url": "/static/media/4.c2c42020.png"
+  },
+  {
+    "revision": "ee31cb696ea7f7a3fcbc0ba0d073f4c5",
+    "url": "/static/media/5.ee31cb69.png"
+  },
+  {
+    "revision": "dccdc64c9d746dc74180bb31305e52ca",
+    "url": "/static/media/6.dccdc64c.png"
+  },
+  {
+    "revision": "e32b6d63acbd8aa222fa6bafc8cc4a18",
+    "url": "/static/media/7.e32b6d63.png"
+  },
+  {
+    "revision": "c4256e7f37d822d99e5aac5f286e3122",
+    "url": "/static/media/8.c4256e7f.png"
+  },
+  {
+    "revision": "7122065b0f241cbe43416c1d58aac95a",
+    "url": "/static/media/9.7122065b.png"
+  },
+  {
+    "revision": "f89ef825a4967e1ad8302ce0188cf599",
+    "url": "/static/media/gerda.f89ef825.jpg"
+  },
+  {
+    "revision": "1eb05f9bc4cb092cd51a0c345fb031a0",
+    "url": "/static/media/logo.1eb05f9b.svg"
+  },
+  {
+    "revision": "9ac18ac9b15a151e9cb6f09fdff66a6a",
+    "url": "/static/media/steps.9ac18ac9.jpg"
+  }
+]);
